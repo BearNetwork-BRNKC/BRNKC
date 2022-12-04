3a70c2bb189a52e95da7c1a2d961db19be8c9948
BearNetwork <chenting.btc@gmail.com>
2022-12-04 23:11:48 +0800

    熊網區塊鏈 BRNKT
    V1

---
+++ contracts/BRNKC.sol
@@ -0,0 +1,63 @@
+//SPDX-License-Identifier: MIT
+//Bear Network - BRNKC - From TAIWAN https://bearnetwork.net
+//Bear Network - [ BRNKC ] 運行在幣安鏈（BSC）上。
+//熊網區塊鏈的目標將圍繞項目與質量、企業、本地化、行業、環境、教育、組織、公益和可持續發展。
+
+pragma solidity >0.5.16 <1.0.0;
+
+import "@openzeppelin/contracts-upgradeable@4.8.0/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.0/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.0/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.0/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.0/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.0/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.0/proxy/utils/UUPSUpgradeable.sol";
+
+/// @custom:security-contact bnkt@bearnetwork.net
+contract BearNetwork is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("Bear Network", "BRNKC");
+        __ERC20Burnable_init();
+        __ERC20Snapshot_init();
+        __Ownable_init();
+        __Pausable_init();
+        __UUPSUpgradeable_init();
+
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
